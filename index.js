7af03d9f6bbf87e9956e1abb59aed96062dc5a52
Albert Thuku <athuku02@gmail.com>
2023-03-23 07:46:48 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(namesArray, eventName){
+  let cardMessage = [];
+
+  for(let i = 0; i<namesArray.length; i++){
+    const message = `Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`;
+    cardMessage.push(message);
+  }
+  return cardMessage;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "suprise"));
+
+function countDown(num){
+  let count = num;
+  while(count >= 0){
+      console.log(num);
+      num--;
+      count--;
+  };
+
+}
+
+console.log(countDown(10));
